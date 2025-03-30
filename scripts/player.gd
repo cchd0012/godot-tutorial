7a740072f12ccb71dff4624b6a6b7f5b5e989458
cchd0012 <chlehd0509@gmail.com>
2025-03-30 10:58:16 +0900

    죽을때 애니메이션을 AnimationPlayer로 처리

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player: CharacterBody2D = $"."
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var game_over = false
 
@@ -13,7 +14,8 @@ var game_over = false
 func _physics_process(delta: float) -> void:
 	
 	if game_over:
-		animated_sprite_2d.play("death")
+		# 재생 후 반복 해제하도록 AnimationPlayer로 설정
+		animation_player.play("death_animation")
 		
 	else:	
 		# Add the gravity.
